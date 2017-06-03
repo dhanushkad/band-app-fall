11f4f9a57ca383e943df8040b618b65af2fe1727
Dhanushka Dolapihilla <DhanushkaRD@Zone24x7.lk>
2017-06-03 13:23:35 +0530

    Addede the motion type listerer from distance listener

--- app/src/main/java/com/microsoft/band/sdk/accelerometer/MainActivity.java
+++ app/src/main/java/com/microsoft/band/sdk/accelerometer/MainActivity.java
@@ -11,25 +11,23 @@ import com.microsoft.band.sensors.SampleRate;
 import com.microsoft.band.sensors.BandGyroscopeEvent;
 import com.microsoft.band.sensors.BandGyroscopeEventListener;
 
+import  com.microsoft.band.sensors.BandDistanceEvent;
+import  com.microsoft.band.sensors.BandDistanceEventListener;
 
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.app.Activity;
 import android.os.AsyncTask;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 
@@ -37,24 +35,39 @@ public class MainActivity extends AppCompatActivity {
     private BandClient client = null;
     private Button btnStart;
     private TextView textStatus;
-
+    private TextView textView;
     /**
      * The file to write the data
      * and the outputstream to use for data writing
-     * **/
+     **/
     File accelGyroFile;
-    FileOutputStream stream;
+    FileOutputStream gyroFileStream;
+
+    private  BandDistanceEventListener mDistantEventListener = new BandDistanceEventListener() {
+        @Override
+        public void onBandDistanceChanged(BandDistanceEvent bandDistanceEvent) {
+            if(bandDistanceEvent != null){
+
+                try{
+
+                    appendTOTextView(bandDistanceEvent.getMotionType().toString());
+                }catch (Exception e){
+
+                    System.out.println(e);
+                }
+            }
+        }
+    };
 
 
     private BandGyroscopeEventListener mGyroscopeEventListener = new BandGyroscopeEventListener() {
         @Override
         public void onBandGyroscopeChanged(final BandGyroscopeEvent event) {
-            if (event != null && stream !=null) {
+            if (event != null && gyroFileStream != null) {
 
-                /*appendToUI(String.format(" X = %.3f \n Y = %.3f\n Z = %.3f \n GX = %.3f\n GY = %.3f\n GZ = %.3f"
+                /*appendTOTextStatus(String.format(" X = %.3f \n Y = %.3f\n Z = %.3f \n GX = %.3f\n GY = %.3f\n GZ = %.3f"
                                , event.getAccelerationX(),
                         event.getAccelerationY(), event.getAccelerationZ(),event.getAngularVelocityX(),event.getAngularVelocityY(),event.getAngularVelocityZ()));*/
-
 
 
                 /**
@@ -75,158 +88,170 @@ public class MainActivity extends AppCompatActivity {
                             String.valueOf(event.getAngularVelocityX()) + "&" +
                             String.valueOf(event.getAngularVelocityY()) + "&" +
                             String.valueOf(event.getAngularVelocityZ()) + "\n";
-                            appendToUI("Entry : "+sensorDateEntry);
+                    appendTOTextStatus("Entry : " + sensorDateEntry);
 
                     //String sensorDateEntry = "PRINT \n";
 
-                    stream.write(sensorDateEntry.getBytes());
+                    gyroFileStream.write(sensorDateEntry.getBytes());
 
                 } catch (IOException e) {
-                   appendToUI("IOx"+e.getMessage());
-                } catch (Exception ex)
-                {
-                    appendToUI("Exception in stream write" + ex.getLocalizedMessage()+ex.toString() + ex.getMessage());
+                    appendTOTextStatus("IOx" + e.getMessage());
+                } catch (Exception ex) {
+                    appendTOTextStatus("Exception in gyroFileStream write" + ex.getLocalizedMessage() + ex.toString() + ex.getMessage());
                 }
 
-            }else {
-                appendToUI("Event or stream is null");
+            } else {
+                appendTOTextStatus("Event or gyroFileStream is null");
             }
         }
     };
 
     /**
      * This returns a file created in the documents directory.
-     *
-     * */
+     */
     public File getFileCreated(String fileName) throws IOException {
         // Get the directory for the user's documents directory.
         File file = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_DOCUMENTS), fileName);
         if (!file.createNewFile()) {
-            appendToUI("Error occured in file writing");
+            appendTOTextStatus("Error occured in file writing");
         }
         return file;
     }
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            textStatus = (TextView) findViewById(R.id.textStatus);
-            btnStart = (Button) findViewById(R.id.btnStart);
-            btnStart.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    new GyroscopeSubscriptionTask().execute();
-                }
-            });
-
-
-            try {
-                accelGyroFile = getFileCreated("accelerometer.txt");
-                appendToUI("File created in" + accelGyroFile.getPath());
-                stream = new FileOutputStream(accelGyroFile,true);
-            }catch (Exception ex){
-                appendToUI("Error file creation " + ex.getMessage());
-            }
-        }
-
-        @Override
-        protected void onResume() {
-            super.onResume();
-
-        }
-
-        @Override
-        protected void onPause() {
-            super.onPause();
-            if (client != null) {
-                try {
-                    client.getSensorManager().unregisterGyroscopeEventListener(mGyroscopeEventListener);
-                } catch (BandIOException e) {
-                    appendToUI("Band Exception"+e.getMessage());
-                }
-            }
-        }
-
-        private class GyroscopeSubscriptionTask extends AsyncTask<Void, Void, Void> {
+        textStatus = (TextView) findViewById(R.id.textStatus);
+        textView = (TextView) findViewById(R.id.textView);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new OnClickListener() {
             @Override
-            protected Void doInBackground(Void... params) {
-                try {
-                    if (getConnectedBandClient()) {
-                        appendToUI("Band is connected.\n");
-                        client.getSensorManager().registerGyroscopeEventListener(mGyroscopeEventListener,SampleRate.MS128);
-                    } else {
-                        appendToUI("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
-                    }
-                } catch (BandException e) {
-                    String exceptionMessage = "";
-                    switch (e.getErrorType()) {
-                        case UNSUPPORTED_SDK_VERSION_ERROR:
-                            exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
-                            break;
-                        case SERVICE_ERROR:
-                            exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
-                            break;
-                        default:
-                            exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
-                            break;
-                    }
-                    appendToUI(exceptionMessage);
+            public void onClick(View v) {
 
-                } catch (Exception e) {
-                    appendToUI(e.getMessage());
-                }
-                return null;
+                new SubscriptionTasks().execute();
+
             }
+        });
+
+
+        try {
+            accelGyroFile = getFileCreated("gyro.txt");
+
+            appendTOTextStatus("File created in" + accelGyroFile.getPath());
+            gyroFileStream = new FileOutputStream(accelGyroFile, true);
+
+        } catch (Exception ex) {
+            appendTOTextStatus("Error file creation " + ex.getMessage());
         }
+    }
 
     @Override
-        protected void onDestroy() {
-            if (client != null) {
-                try {
-                    client.disconnect().await();
-                    stream.close();
+    protected void onResume() {
+        super.onResume();
 
-                } catch (InterruptedException e) {
-                    // Do nothing as this is happening during destroy
-                } catch (BandException e) {
-                    // Do nothing as this is happening during destroy
-                } catch (IOException e) {
+    }
 
-                }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (client != null) {
+            try {
+                client.getSensorManager().unregisterGyroscopeEventListener(mGyroscopeEventListener);
+                client.getSensorManager().unregisterDistanceEventListener(mDistantEventListener);
+            } catch (BandIOException e) {
+                appendTOTextStatus("Band Exception" + e.getMessage());
             }
-            super.onDestroy();
         }
+    }
 
-        private void appendToUI(final String string) {
-            this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    textStatus.setText(string);
+    private class SubscriptionTasks extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                if (getConnectedBandClient()) {
+                    appendTOTextStatus("Band is connected.\n");
+                    client.getSensorManager().registerGyroscopeEventListener(mGyroscopeEventListener, SampleRate.MS128);
+                    client.getSensorManager().registerDistanceEventListener(mDistantEventListener);
+                } else {
+                    appendTOTextStatus("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
                 }
-            });
-        }
+            } catch (BandException e) {
+                String exceptionMessage = "";
+                switch (e.getErrorType()) {
+                    case UNSUPPORTED_SDK_VERSION_ERROR:
+                        exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
+                        break;
+                    case SERVICE_ERROR:
+                        exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
+                        break;
+                    default:
+                        exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
+                        break;
+                }
+                appendTOTextStatus(exceptionMessage);
 
-        private boolean getConnectedBandClient() throws InterruptedException, BandException {
-            if (client == null) {
-                BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
-                if (devices.length == 0) {
-                    appendToUI("Band isn't paired with your phone.\n");
-                    return false;
-                }
-                client = BandClientManager.getInstance().create(getBaseContext(), devices[0]);
-            } else if (ConnectionState.CONNECTED == client.getConnectionState()) {
-                return true;
+            } catch (Exception e) {
+                appendTOTextStatus(e.getMessage());
             }
-
-            appendToUI("Band is connecting...\n");
-            return ConnectionState.CONNECTED == client.connect().await();
+            return null;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (client != null) {
+            try {
+                client.disconnect().await();
+                gyroFileStream.close();
+
+            } catch (InterruptedException e) {
+                // Do nothing as this is happening during destroy
+            } catch (BandException e) {
+                // Do nothing as this is happening during destroy
+            } catch (IOException e) {
+
+            }
+        }
+        super.onDestroy();
+    }
+
+    private void appendTOTextStatus(final String string) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textStatus.setText(string);
+            }
+        });
+    }
+
+    private void appendTOTextView(final String string) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(string);
+            }
+        });
+    }
+
+    private boolean getConnectedBandClient() throws InterruptedException, BandException {
+        if (client == null) {
+            BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
+            if (devices.length == 0) {
+                appendTOTextStatus("Band isn't paired with your phone.\n");
+                return false;
+            }
+            client = BandClientManager.getInstance().create(getBaseContext(), devices[0]);
+        } else if (ConnectionState.CONNECTED == client.getConnectionState()) {
+            return true;
+        }
+
+        appendTOTextStatus("Band is connecting...\n");
+        return ConnectionState.CONNECTED == client.connect().await();
+    }
 
 }
 
